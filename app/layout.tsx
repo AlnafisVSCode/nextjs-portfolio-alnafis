5f1631bbcb7927b4242eb1093e34a65a9c6a0cb9
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-04-08 22:16:45 +0100

    XX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={"${inter.className} bg-gray-50 text-gray-950 relative"}>
+			<body
+				className={
+					"${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]"
+				}>
 				<div
 					className="bg-[#dddbc3] absolute
         top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]
@@ -28,8 +32,8 @@ export default function RootLayout({
         top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
         sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
         "></div>
-				<div></div>
 
+				<Header />
 				{children}
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Header from "@/components/header";
 
 export default function Home() {
 	return <main className=""></main>;
