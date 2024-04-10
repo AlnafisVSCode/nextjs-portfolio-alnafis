9d6de40445dea1757060ddd05812fcfd5e8b5803
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-04-10 19:39:27 +0100

    Section 1 Complete

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,24 +1,98 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
 	return (
-		<section>
+		<section className="mb: 28 max-w-[50rem] text-center sm:mb-0">
+			{/* Photo */}
 			<div className="flex items-center justify-center">
 				<div className="relative">
-					<Image
-						src="/Upscaled Leather Me.jpg"
-						width={192}
-						height={192}
-						quality={95}
-						alt="Alnafis Portrait"
-						priority={true}
-						className="h-24 w-24  rounded-full object-cover border-[0.35rem] border-white  shadow-xl sm:h-32 sm:w-32"
-					/>
+					<motion.div
+						initial={{ opacity: 0, scale: 0 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{ type: "tween", duration: 0.2 }}>
+						<Image
+							src="/Upscaled Leather Me.jpg"
+							width={192}
+							height={192}
+							quality={95}
+							alt="Alnafis Portrait"
+							priority={true}
+							className="h-24 w-24  rounded-full object-cover border-[0.35rem] border-white  shadow-xl sm:h-32 sm:w-32"
+						/>
+					</motion.div>
 
-					<span className=" absolute bottom-0 text-4xl">😁</span>
+					<motion.span
+						className=" absolute bottom-0 text-4xl"
+						initial={{ opacity: 0, scale: 0 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{
+							type: "spring",
+							stiffness: 125,
+							delay: 0.5,
+							duration: 0.6,
+						}}>
+						😁
+					</motion.span>
 				</div>
 			</div>
+
+			{/* intro description */}
+			<motion.p
+				className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}>
+				<span className="font-bold">Hello, I'm Alnafis.</span> I'm a{" "}
+				<span className="font-bold">software developer</span> as a{" "}
+				<span className="font-bold">recent</span> graduate. I enjoy building{" "}
+				<span className="italic">sites & apps</span>. My focus is{" "}
+				<span className="underline">React (Next.js)</span>.
+			</motion.p>
+
+			{/* Social Links */}
+			<motion.div
+				className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.1,
+				}}>
+				<Link
+					href="#contact"
+					className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+					Contact me here{" "}
+					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+				</Link>
+
+				<a
+					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-Black/20 dark:bg-white/10"
+					href="/AlnafisChowdhury_Resume.pdf"
+					download>
+					Download CV{" "}
+					<HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+				</a>
+
+				<a
+					className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://linkedin.com"
+					target="_blank">
+					<BsLinkedin />
+				</a>
+
+				<a
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://github.com"
+					target="_blank">
+					<FaGithubSquare />
+				</a>
+			</motion.div>
 		</section>
 	);
 }
