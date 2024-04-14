3895ac5514c4de98e09637e40eef36d71e3f6f2c
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-04-14 22:18:43 +0100

    Project Section Complete - Motion added, server v client optimisation components

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Sectionheadings from "./section-headings";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
+import Project from "./project";
 
 export default function Projects() {
 	return (
@@ -15,42 +15,6 @@ export default function Projects() {
 					</React.Fragment>
 				))}
 			</div>
-		</section>
-	);
-}
-
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-	return (
-		<section
-			className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
-     mb-3 sm:mb-8 last:mb-0 even:pl-8">
-			{/* Before Image Section */}
-			<div
-				className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 
-      sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
-				<h3 className="text-3xl">{title}</h3>
-				<p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-				<ul className=" flex flex-wrap mt-4 gap-2 sm:mt-auto">
-					{tags.map((tag, index) => (
-						<li
-							className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full "
-							key={index}>
-							{tag}
-						</li>
-					))}
-				</ul>
-			</div>
-
-			{/* Image Section */}
-			<Image
-				src={imageUrl}
-				alt="The Project I worked on"
-				quality={95}
-				className="absolute top-8 -right-40
-        w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40"
-			/>
 		</section>
 	);
 }
--- components/section-headings.tsx
+++ components/section-headings.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type childrenprops = { children: React.ReactNode };
 
 export default function Sectionheadings({ children }: childrenprops) {
-	return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+	return (
+		<h2 className="text-3xl font-medium capitalize mb-8 text-center">
+			{children}
+		</h2>
+	);
 }
