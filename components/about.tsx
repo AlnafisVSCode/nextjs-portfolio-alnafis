73e7b86d5165789ddad4ca17e58da4dc6910151b
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-06-04 02:21:52 +0100

    Changing some info

--- components/about.tsx
+++ components/about.tsx
@@ -20,9 +20,9 @@ export default function About() {
 			<Sectionheadings> About me</Sectionheadings>
 			<p className="mb-3">
 				After graduating with a degree in{" "}
-				<span className="font-medium">Accounting</span>, I decided to pursue my
-				passion for programming. I enrolled in a coding bootcamp and learned{" "}
-				<span className="font-medium">full-stack web development</span>.{" "}
+				<span className="font-medium">Software Engineering</span>, I decided to
+				pursue my passion for Data. I enrolled in my masters in Big Data Science{" "}
+				<span className="font-medium">full-stack web development.</span>.{" "}
 				<span className="italic">My favorite part of programming</span> is the
 				problem-solving aspect. I <span className="underline">love</span> the
 				feeling of finally figuring out a solution to a problem. My core stack
