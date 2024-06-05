55476625b49ee75337223ca6c3f94e387cd45ae2
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2024-06-05 20:47:08 +0100

    removed bloat

--- lib/data.tsx
+++ lib/data.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 import bioApp from "@/public/bioapp.png";
-import testEyes from "@/public/testeyes.png";
 import testEye from "@/public/testeye.jpg";
 import AudioOutput from "@/public/audiooutput.png";
 
